1fd8a5e12b83e7e8eb0ed7d77c7c49fc7c9948b9
MpackVinicius <vinicius.tiberio@micropack.com.br>
2025-10-21 16:32:32 -0300

    fix: corrige import @tanstack/react-query

--- mobile/src/screens/admin/ProfessoresScreen.tsx
+++ mobile/src/screens/admin/ProfessoresScreen.tsx
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput, Alert, RefreshControl, Dimensions } from 'react-native';
-import { useQuery, useMutation, useQueryClient } from '@tantml:react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { adminService } from '../../services/api';
 import { useState } from 'react';
 
